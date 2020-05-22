7ad94abad1b6aa97540bed9bc44c2980f8b73589
dora wu <chiha8888@gmail.com>
2020-05-22 22:35:52 +0800

    update WGAN-GP training function

--- lab6/main.py
+++ lab6/main.py
@@ -11,24 +11,25 @@ device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 z_dim=100
 c_dim=100
 image_shape=(64,64,3)
-epochs=200
+epochs=1000
 lr=0.0002
 batch_size=64
-test_path=os.path.join('dataset','test.json')
-
+LAMBDA=10   # gradient penalty weight in WGAN-GP
+n_critic=5  # number of training steps for discriminator per iter
 
 if __name__=='__main__':
 
     # load training data
     dataset_train=CLEVRDataset(img_path='iclevr',json_path=os.path.join('dataset','train.json'))
-    loader_train=DataLoader(dataset_train,batch_size=batch_size,shuffle=True,num_workers=2)
+    loader_train=DataLoader(dataset_train,batch_size=batch_size,shuffle=True,num_workers=4)
 
     # create generate & discriminator
     generator=Generator(z_dim,c_dim).to(device)
     discrimiator=Discriminator(image_shape,c_dim).to(device)
-    generator.weight_init(mean=0,std=0.02)
-    discrimiator.weight_init(mean=0,std=0.02)
+    # no need to init in WGAN-GP
+    #generator.weight_init(0,0.02)
+    #discrimiator.weight_init(0,0.02)
 
     # train
-    train(loader_train,generator,discrimiator,z_dim,c_dim,epochs,lr,test_path)
+    train(loader_train,generator,discrimiator,z_dim,epochs,lr,LAMBDA,n_critic)
 
--- lab6/train.py
+++ lab6/train.py
@@ -4,21 +4,19 @@ import numpy as np
 import copy
 import os
 
-from util import get_test_conditions
+from util import get_test_conditions,save_image
 from evaluator import EvaluationModel
 device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train(dataloader,g_model,d_model,z_dim,c_dim,epochs,lr,test_path):
+def train(dataloader,g_model,d_model,z_dim,epochs,lr,LAMBDA,n_critic):
     """
     :param z_dim: 100
-    :param c_dim: 100
     """
-    Criterion=nn.BCELoss()
     optimizer_g=torch.optim.Adam(g_model.parameters(),lr,betas=(0.5,0.99))
     optimizer_d=torch.optim.Adam(d_model.parameters(),lr,betas=(0.5,0.99))
     evaluation_model=EvaluationModel()
 
-    test_conditions=get_test_conditions(test_path).to(device)
+    test_conditions=get_test_conditions().to(device)
     fixed_z = random_z(len(test_conditions), z_dim).to(device)
     best_score = 0
 
@@ -32,43 +30,44 @@ def train(dataloader,g_model,d_model,z_dim,c_dim,epochs,lr,test_path):
             images = images.to(device)
             conditions = conditions.to(device)
 
-            real = torch.ones(batch_size).to(device)
-            fake = torch.zeros(batch_size).to(device)
             """
             train discriminator
             """
             optimizer_d.zero_grad()
 
             # for real images
-            predicts = d_model(images, conditions)
-            loss_real = Criterion(predicts, real)
+            real_predicts = d_model(images, conditions)
             # for fake images
             z = random_z(batch_size, z_dim).to(device)
             gen_imgs = g_model(z,conditions)
-            predicts = d_model(gen_imgs.detach(), conditions)
-            loss_fake = Criterion(predicts, fake)
+            fake_predicts = d_model(gen_imgs.detach(), conditions)
+            # for gradient penalty
+            gradient_penalty=calc_gradient_penalty(d_model,images,gen_imgs,conditions)
+
             # bp
-            loss_d = loss_real + loss_fake
+            # WGAN-GP define real=-1 , fake=+1
+            loss_d = -torch.mean(real_predicts)+torch.mean(fake_predicts)+LAMBDA*gradient_penalty
             loss_d.backward()
             optimizer_d.step()
 
             """
-            train generator
+            train generator every 5 iterations
             """
-            for _ in range(4):
+            if i%n_critic==0:
                 optimizer_g.zero_grad()
 
                 z = random_z(batch_size, z_dim).to(device)
-                gen_imgs = g_model(z, conditions)
-                predicts = d_model(gen_imgs,conditions)
-                loss_g = Criterion(predicts,real)
+                ran_conditions = random_conditions(batch_size).to(device)
+                gen_imgs = g_model(z, ran_conditions)
+                fake_predicts = d_model(gen_imgs,conditions)
+
                 # bp
+                loss_g = -torch.mean(fake_predicts)
                 loss_g.backward()
                 optimizer_g.step()
-
-            print(f'epoch{epoch} {i}/{len(dataloader)}  loss_g: {loss_g.item():.3f}  loss_d: {loss_d.item():.3f}')
-            total_loss_g+=loss_g.item()
-            total_loss_d+=loss_d.item()
+                print(f'epoch{epoch} {i}/{len(dataloader)}  loss_g: {loss_g.item():.3f}  loss_d: {loss_d.item():.3f}')
+                total_loss_d+=loss_d.item()
+                total_loss_g+=loss_g.item()
 
         # evaluate
         g_model.eval()
@@ -79,21 +78,40 @@ def train(dataloader,g_model,d_model,z_dim,c_dim,epochs,lr,test_path):
         if score>best_score:
             best_score=score
             best_model_wts=copy.deepcopy(g_model.state_dict())
-            torch.save(best_model_wts,os.path.join('models',f'score{score:.2f}.pt'))
-        print(f'avg loss_g: {total_loss_g/len(dataloader):.3f}  avg_loss_d: {total_loss_d/len(dataloader):.3f}')
+            torch.save(best_model_wts,os.path.join('models',f'epoch{epoch}_score{score:.2f}.pt'))
+        print(f'avg loss_g: {total_loss_g/(len(dataloader)//n_critic+1):.3f}  avg_loss_d: {total_loss_d/(len(dataloader)//n_critic+1):.3f}')
         print(f'testing score: {score:.2f}')
         print('---------------------------------------------')
+        # savefig
+        save_image(gen_imgs, os.path.join('results',f'epoch{epoch}.png'), nrow=8, normalize=True)
+
+def calc_gradient_penalty(netD, real_data, fake_data, conditions):
+    # print "real_data: ", real_data.size(), fake_data.size()
+    batch_size = real_data.shape[0]
+    alpha = torch.rand(batch_size, 1, 1, 1)
+    alpha = alpha.expand(real_data.shape).to(device)
+    interpolates = alpha * real_data + ((1 - alpha) * fake_data)
+
+    disc_interpolates = netD(interpolates,conditions)
+
+    gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
+                              create_graph=True, retain_graph=True, only_inputs=True)[0]
+    gradients = gradients.view(gradients.size(0), -1)
+
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+    return gradient_penalty
 
 def random_z(batch_size,z_dim):
     return torch.randn(batch_size,z_dim)
 
-def random_labels(batch_size,c_dim):
+def random_conditions(batch_size):
     pick_num=np.random.randint(1,4)
     pick=np.random.choice(24,pick_num,replace=False)
-    labels=torch.zeros(batch_size,c_dim)
+    re=torch.zeros(batch_size,24)
     for i in pick:
-        labels[i]=1.
-    return labels
+        re[i]=1.
+    return re
 
 
 
